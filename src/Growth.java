a8c6d2e9846b9cedbb8b41c206c6ae50bad49f96
Joshua Makula <makulajosh@gmail.com>
2023-06-06 19:40:23 +0300

    Setup projects

---
+++ src/Growth.java
@@ -0,0 +1,15 @@
+
+public class Growth {
+
+	public static void main(String[] args) {
+		// y = a(1 + r)^x
+		// a = initial value
+		// r = growth rate
+		// x = time interval
+		
+		System.out.println("Let's calculate subs!");
+		System.out.println("This program uses y = a(1 + r)^x");
+
+	}
+
+}
